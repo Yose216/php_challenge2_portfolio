2c1e6f4339441664410b46479df89f65f0bf7adf
Yose <youness;chetoui@gmail.com>
2016-09-01 12:16:47 +0200

    Rajout id pour ancre

--- index.php
+++ index.php
@@ -12,47 +12,47 @@
                 <nav class="navbar navbar-inverse navbar-fixed-top">
                     <div class="container-fluid">
                         <div class="navbar-header">
-                            <a class="navbar-brand" href="#">Le Portfolio de Youness</a>
+                            <a class="navbar-brand" href="#home">Le Portfolio de Youness</a>
                         </div>
                         <ul class="nav navbar-nav navbar-right">
                             <li class="active"><a href="#">Home</a></li>
-                            <li><a href="#">HTML5/CSS3</a></li>
-                            <li><a href="#">JavaScript</a></li>
-                            <li><a href="#">Jquery</a></li>
-                            <li><a href="#">PHP</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Contact</a></li>
+                            <li><a href="#html">HTML5/CSS3</a></li>
+                            <li><a href="#JS">JavaScript</a></li>
+                            <li><a href="#Jquery">Jquery</a></li>
+                            <li><a href="#PHP">PHP</a></li>
+                            <li><a href="#Me">About</a></li>
+                            <li><a href="#Contact">Contact</a></li>
                         </ul>
                     </div>
                 </nav>
             </header>
             
             <div>
-                <h2>Portfolio You</h2>
+                <h2 id="home">Portfolio You</h2>
             </div>
             
             <div>
-                <h2>HTML5/CSS3</h2>
+                <h2 id="html">HTML5/CSS3</h2>
             </div>
             
             <div>
-                <h2>JavaScript</h2>
+                <h2 id="JS">JavaScript</h2>
             </div>
 
             <div>
-                <h2>Jquery</h2>
+                <h2 id="Jquery">Jquery</h2>
             </div>
 
             <div>
-                <h2>PHP</h2>
+                <h2 id="PHP">PHP</h2>
             </div>
             
             <div>
-                <h2>About</h2>
+                <h2 id="Me">About</h2>
             </div>
 
             <div>
-                <h2>Contact</h2>
+                <h2 id="Contact">Contact</h2>
             </div>
             
             <script src="JS/jquery-2.2.2.min.js" type="text/javascript"></script>
