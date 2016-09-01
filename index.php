24a1aa809b22fb71aa5fce5eb3b4e95f41e53469
Yose <youness;chetoui@gmail.com>
2016-09-01 12:13:57 +0200

    First commit, squellete dossier

---
+++ index.php
@@ -0,0 +1,62 @@
+<Doctype html>
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>Mon portfolio</title>
+            <link rel="stylesheet" href="CSS/styles.css"/>
+            <link rel="stylesheet" type="text/css" href="CSS/Bootstrap/css/bootstrap.css"/>    
+        </head>
+        
+        <body>
+            <header>
+                <nav class="navbar navbar-inverse navbar-fixed-top">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">Le Portfolio de Youness</a>
+                        </div>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">HTML5/CSS3</a></li>
+                            <li><a href="#">JavaScript</a></li>
+                            <li><a href="#">Jquery</a></li>
+                            <li><a href="#">PHP</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            
+            <div>
+                <h2>Portfolio You</h2>
+            </div>
+            
+            <div>
+                <h2>HTML5/CSS3</h2>
+            </div>
+            
+            <div>
+                <h2>JavaScript</h2>
+            </div>
+
+            <div>
+                <h2>Jquery</h2>
+            </div>
+
+            <div>
+                <h2>PHP</h2>
+            </div>
+            
+            <div>
+                <h2>About</h2>
+            </div>
+
+            <div>
+                <h2>Contact</h2>
+            </div>
+            
+            <script src="JS/jquery-2.2.2.min.js" type="text/javascript"></script>
+            <script src="JS/script.js" type="text/javascript"></script>  
+        </body>
+    </html>
+    
