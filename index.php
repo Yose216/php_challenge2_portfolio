98ee7123c94fad602f74d737224491186f4e1ce6
Yose <youness;chetoui@gmail.com>
2016-09-01 14:02:59 +0200

    Rajout image language + margin top pour le premier titre

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                             <li class="active"><a href="#">Home</a></li>
                             <li><a href="#html">HTML5/CSS3</a></li>
                             <li><a href="#JS">JavaScript</a></li>
-                            <li><a href="#Jquery">Jquery</a></li>
+                            <li><a href="#jQuery">jQuery</a></li>
                             <li><a href="#PHP">PHP</a></li>
                             <li><a href="#Me">About</a></li>
                             <li><a href="#Contact">Contact</a></li>
@@ -27,34 +27,36 @@
                 </nav>
             </header>
             
-            <div>
-                <h2 id="home">Portfolio You</h2>
-            </div>
-            
-            <div>
-                <h2 id="html">HTML5/CSS3</h2>
-            </div>
-            
-            <div>
-                <h2 id="JS">JavaScript</h2>
-            </div>
+            <div class="container portfolio">    
+                <div class="home">
+                    <h2 id="home">Portfolio You</h2>
+                        <p>Bienvenue Sur Mon Portfolio, Vous Retrouverez Tout mon Travail içi!</p>
+                </div>
+                
+                <div>
+                    <h2 id="html">HTML5/CSS3 <img src="Images/html_css.png"/></h2>
+                </div>
+                
+                <div>
+                    <h2 id="JS">JavaScript <img src="Images/js.png"/></h2>
+                </div>
 
-            <div>
-                <h2 id="Jquery">Jquery</h2>
-            </div>
+                <div>
+                    <h2 id="jQuery">jQuery <img src="Images/JQuery.png"/></h2>
+                </div>
 
-            <div>
-                <h2 id="PHP">PHP</h2>
-            </div>
-            
-            <div>
-                <h2 id="Me">About</h2>
-            </div>
+                <div>
+                    <h2 id="PHP">PHP <img src="Images/php.png"/></h2>
+                </div>
+                
+                <div>
+                    <h2 id="Me">About</h2>
+                </div>
 
-            <div>
-                <h2 id="Contact">Contact</h2>
+                <div>
+                    <h2 id="Contact">Contact</h2>
+                </div>
             </div>
-            
             <script src="JS/jquery-2.2.2.min.js" type="text/javascript"></script>
             <script src="JS/script.js" type="text/javascript"></script>  
         </body>
