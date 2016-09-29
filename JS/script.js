8ffa81811aa1147d4d9a0515bdc83a60dd8bfb58
Yose <youness;chetoui@gmail.com>
2016-09-29 09:58:03 +0200

    add a toggle whith jquery for the carousel

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,44 @@
-
 //Script du carrousel
 $(document).ready(function () {
-        var carousel = $(".carousel").waterwheelCarousel({
-          flankingItems: 3,
-          movingToCenter: function ($item) {
-            $('#callback-output').prepend('movingToCenter: ' + $item.attr('id') + '<br/>');
-          },
-          movedToCenter: function ($item) {
-            $('#callback-output').prepend('movedToCenter: ' + $item.attr('id') + '<br/>');
-          },
-          movingFromCenter: function ($item) {
-            $('#callback-output').prepend('movingFromCenter: ' + $item.attr('id') + '<br/>');
-          },
-          movedFromCenter: function ($item) {
-            $('#callback-output').prepend('movedFromCenter: ' + $item.attr('id') + '<br/>');
-          },
-          clickedCenter: function ($item) {
-            $('#callback-output').prepend('clickedCenter: ' + $item.attr('id') + '<br/>');
-          }
-        });
+	var carousel = $(".carousel").waterwheelCarousel({
+		flankingItems: 3,
+		movingToCenter: function ($item) {
+			$('#callback-output').prepend('movingToCenter: ' + $item.attr('id') + '<br/>');
+		},
+		movedToCenter: function ($item) {
+			$('#callback-output').prepend('movedToCenter: ' + $item.attr('id') + '<br/>');
+		},
+		movingFromCenter: function ($item) {
+			$('#callback-output').prepend('movingFromCenter: ' + $item.attr('id') + '<br/>');
+		},
+		movedFromCenter: function ($item) {
+			$('#callback-output').prepend('movedFromCenter: ' + $item.attr('id') + '<br/>');
+		},
+		clickedCenter: function ($item) {
+			$('#callback-output').prepend('clickedCenter: ' + $item.attr('id') + '<br/>');
+		}
+	});
 
-        $('#prev').bind('click', function () {
-          carousel.prev();
-          return false
-        });
+	$('#prev').bind('click', function () {
+		carousel.prev();
+		return false
+	});
 
-        $('#next').bind('click', function () {
-          carousel.next();
-          return false;
-        });
+	$('#next').bind('click', function () {
+		carousel.next();
+		return false;
+	});
 
-        $('#reload').bind('click', function () {
-          newOptions = eval("(" + $('#newoptions').val() + ")");
-          carousel.reload(newOptions);
-          return false;
-        });
+	$('#reload').bind('click', function () {
+		newOptions = eval("(" + $('#newoptions').val() + ")");
+		carousel.reload(newOptions);
+		return false;
+	});
 
-      });
-	  
-	  	
+});
+
+$(document).ready(function(){
+    $(".chevron").click(function(){
+        $("#carouselHtml").slideToggle("slow");
+    });
+});
